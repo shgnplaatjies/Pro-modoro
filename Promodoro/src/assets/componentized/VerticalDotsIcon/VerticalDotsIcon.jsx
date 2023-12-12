6c92e2474006c3641d95e15c3b0f0e05f2a43535
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2023-12-12 02:46:06 +0200

    Adds new assets

---
+++ Promodoro/src/assets/componentized/VerticalDotsIcon/VerticalDotsIcon.jsx
@@ -0,0 +1,16 @@
+function VerticalDotsIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="vertical-dots-icon"
+      className="vertical-dots-icon"
+      viewBox="0 0 24 24"
+    >
+      <circle cx="12" cy="2" r="2" />
+      <circle cx="12" cy="12" r="2" />
+      <circle cx="12" cy="22" r="2" />
+    </svg>
+  );
+}
+
+export default VerticalDotsIcon;
---
+++ Promodoro/src/assets/sounds/SimpleSounds/SimpleSounds.js
@@ -0,0 +1,4 @@
+export const playAlarm = () => {
+  const alarmSound = new Audio("./roosterSound.wav");
+  alarmSound.play();
+};
