42a70fba0de831ee507d63f140697892f735ccfb
Shagan Plaatjies <plaatjiesshagan@gmail.com>
2024-02-08 03:16:57 +0200

    Adds responsive width clamping app-wide.

--- Promodoro/src/pages/Home/Home.jsx
+++ Promodoro/src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="home-screen">
+    <div className="home-container">
       <section className="pomodoro-section">
         <AppHeader />
         <Pomodoro />
